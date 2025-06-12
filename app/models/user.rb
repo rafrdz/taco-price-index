7172275a507172c43837a0839e0e85458e811604
Tracy Chacon <tracy.chacon.00@gmail.com>
2025-06-12 18:43:25 -0500

    feat:Set up dummy email configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
 
+  # Rails login
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+  validates :email_address, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP}
+  validates :password, length: { minimum: 6 }, if: -> {}
 end
