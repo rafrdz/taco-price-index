7321a058413955441c00c1ec4d37162c2679fbb7
Graham Nessler <graham.nessler@gmail.com>
2025-06-28 16:48:07 -0500

    Update photo model: add validations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,6 @@
 class Photo < ApplicationRecord
   belongs_to :taco
+
+  validates :url, presence: true
+  validates :url, format: { with: URI::regexp(%w[http https]), message: "must be a valid URL" }
 end
