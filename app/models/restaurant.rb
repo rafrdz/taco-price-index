f8fb456006528b792cbe821b042e7cfabfa8a858
Graham Nessler <graham.nessler@gmail.com>
2025-06-29 14:08:59 -0500

    add validations to restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,6 +7,20 @@ class Restaurant < ApplicationRecord
   has_many :user_favorites, dependent: :destroy
   has_many :favorited_by, through: :user_favorites, source: :user
 
+  # Validations
+  validates :name, presence: true
+  validates :street_address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip, presence: true
+  validates :latitude, presence: true, numericality: true
+  validates :longitude, presence: true, numericality: true
+  validates :phone, presence: true
+  validates :website, format: { with: URI::regexp(%w[http https]), message: "must be a valid URL" }, allow_blank:  true
+  validates :google_rating, numericality: true
+  validates :google_price_level, numericality: true
+  validates :google_user_ratings_total, numericality: true, allow_nil: true
+
   # Geocoding
   geocoded_by :full_address
   after_validation :geocode, if: ->(obj) { obj.full_address.present? && (obj.street_address_changed? || obj.city_changed? || obj.state_changed? || obj.zip_changed?) }
