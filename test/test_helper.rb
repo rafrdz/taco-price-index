2223c4d61994abd7c472d4284f05b7900145a42d
Rafael Rodriguez <rrodriguez1989@gmail.com>
2025-07-01 21:41:08 -0500

    Fix user profile. Add sign_in and sign_out helpers.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require_relative "helpers/session_test_helper"
 
 module ActiveSupport
   class TestCase
@@ -11,5 +12,6 @@ module ActiveSupport
     fixtures :all
 
     # Add more helper methods to be used by all tests here...
+    include SessionTestHelper
   end
 end
