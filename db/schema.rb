07b9b834f85288306bd91c2c3b78a8afbc385e62
Graham Nessler <graham.nessler@gmail.com>
2025-08-22 16:47:25 -0500

    Improve restaurant card design on Map page.
    - Add `cuisine_type` column to `restaurants` via migration.
    - Implement `pinRestaurantOnMap` method in `map_controller.js` for enhanced mapping.
    - Revamp restaurant list UI with detailed cards for improved UX.
    - Update styles for better responsiveness and aesthetics.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_01_220559) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_22_213145) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -69,6 +69,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_01_220559) do
     t.text "business_hours"
     t.text "description"
     t.integer "user_favorites_count", default: 0, null: false
+    t.string "cuisine_type"
     t.index ["user_favorites_count"], name: "index_restaurants_on_user_favorites_count"
   end
 
@@ -137,9 +138,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_01_220559) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
+    t.boolean "admin", default: false, null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
